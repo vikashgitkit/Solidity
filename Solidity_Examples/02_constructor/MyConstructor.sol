70cd50959a1bf5ec9362137630952c91afa5d6a1
Webskitters <vikashofficial9@gmail.com>
2024-07-26 17:51:11 +0530

    Contructor

---
+++ Solidity_Examples/02_constructor/MyConstructor.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+//
+// https://cryptomarketpool.com/constructor-in-solidity-smart-contracts/
+pragma solidity ^0.8.20;
+
+contract MyContructor {
+    uint256 public x;
+    uint256 public y;
+    address public owner;
+    uint256 public createdAt;
+
+    constructor(uint256 _x, uint256 _y) {
+        x = _x;
+        y = _y;
+        owner = msg.sender;
+        createdAt = block.timestamp;
+    }
+}
