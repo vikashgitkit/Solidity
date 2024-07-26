27969e233fd2f13f8af3dfb960b6489022ea11a0
Webskitters <vikashofficial9@gmail.com>
2024-07-26 17:36:47 +0530

    Operators

---
+++ Solidity_Examples/01_data_types/02_SolidityOperators.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+//
+// https://cryptomarketpool.com/data-types-in-solidity-smart-contracts/
+
+pragma solidity ^0.8.20;
+
+contract SolidityOperator {
+    uint256 public variable_add = 70;
+    uint256 public variable_sub = 50;
+    uint256 public variable_mul = 25;
+    uint256 public variable_div = 80;
+    uint256 public variable_mod = 68;
+
+    function getResults() public {
+        variable_add += 25;
+        variable_sub -= 30;
+        variable_mul *= 25;
+        variable_div /= 5;
+        variable_mod %= 60;
+        return;
+    }
+}
