cb0825c3382e159999aa73ab2911ad2abca16ff9
Webskitters <vikashofficial9@gmail.com>
2024-07-30 19:23:57 +0530

    For loop

---
+++ Solidity_Examples/11_forLoop/MyContract.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+//
+// https://cryptomarketpool.com/for-loop
+pragma solidity ^0.8.20;
+
+contract MyContract{
+    uint256 public loopCount;
+
+    function doLoop(uint256 n) public{
+        for(uint256 i = 0; i < n; i++){
+            loopCount += 1;
+        }
+    }
+}
