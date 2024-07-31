c7e08a70aae53f1b0e370b7b0438a1fa72d81856
Webskitters <vikashofficial9@gmail.com>
2024-07-31 17:09:48 +0530

    Mapping

---
+++ Solidity_Examples/14_mapping/MyContract.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+//
+// https://cryptomarketpool.com/how-to-create-a-map-in-a-solidity-smart-contract/
+
+pragma solidity ^0.8.20;
+
+contract MyContract {
+    mapping(address => bool) public myMap;
+
+    function set(address _addr, bool i) public {
+        myMap[_addr] = i;
+    }
+
+    function get(address _addr) public view returns (bool) {
+        return myMap[_addr];
+    }
+
+    function remover(address _addr) public {
+        delete myMap[_addr];
+        
+    }
+
+}
