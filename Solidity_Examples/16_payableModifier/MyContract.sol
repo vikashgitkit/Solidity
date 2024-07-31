a784d2678771b6ada9765c859a3c68feb04d6a9f
Webskitters <vikashofficial9@gmail.com>
2024-07-31 17:22:00 +0530

    payable modifier

---
+++ Solidity_Examples/16_payableModifier/MyContract.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+//
+// https://cryptomarketpool.com/payable-functions
+
+pragma solidity ^0.8.0;
+
+contract MyContract{
+
+    address payable public owner;
+
+    constructor() payable {
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not an Onwer..");
+        _;
+
+    }
+
+    function withdraw(uint256 amount) public onlyOwner{
+        owner.transfer(amount);
+    }
+
+    function transfer(address payable _to, uint256 amount) public onlyOwner {
+        _to.transfer(amount);
+    }
+}
