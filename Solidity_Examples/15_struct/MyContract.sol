5038343cefa018108193af87897a80aa152269b2
Webskitters <vikashofficial9@gmail.com>
2024-07-31 17:17:55 +0530

    Struct

---
+++ Solidity_Examples/15_struct/MyContract.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+//
+// https://cryptomarketpool.com/struct
+
+pragma solidity ^0.8.20;
+
+contract MyContract{
+
+struct Todo {
+    string text;
+    bool completed;
+}
+
+Todo[] public todos;
+
+function create(string memory _text) public {
+    todos.push(Todo(_text, false));
+}
+
+function get(uint256 _index) public view returns(string memory, bool) {
+    Todo storage todo = todos[_index];
+    return (todo.text, todo.completed);
+}
+
+}
