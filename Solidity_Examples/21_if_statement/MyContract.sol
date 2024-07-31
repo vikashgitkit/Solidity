cb7ad8ae608bb8067dfd3f862fc91b5ca5ec90dd
Webskitters <vikashofficial9@gmail.com>
2024-07-31 19:44:39 +0530

    If Statement

---
+++ Solidity_Examples/21_if_statement/MyContract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+//
+// https://cryptomarketpool.com/how-to-write-an-if-statement-in-solidity/
+
+pragma solidity ^0.8.20;
+
+contract MyContract{
+
+    string public symbol;
+
+    function symbolNum(uint256 _symbolNum) public {
+        if(_symbolNum == 1) {
+            symbol = "BTC";
+
+        } else if(_symbolNum == 2) {
+            symbol = "ETH";
+            
+                    } else {
+                        symbol = "CRPCOIN";
+                    }
+    }
+}
