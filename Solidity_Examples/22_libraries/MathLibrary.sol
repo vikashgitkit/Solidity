a4aacfe5c2dc4e05df7dd049ccc3733d795ff2a4
Webskitters <vikashofficial9@gmail.com>
2024-08-01 11:05:46 +0530

    Libraries

---
+++ Solidity_Examples/22_libraries/MathLibrary.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+//
+// https://cryptomarketpool.com/libraries-in-solidity/
+
+pragma solidity ^0.8.20;
+
+library MathLibrary{
+
+function multiply(uint256 a, uint256 b) public view returns(uint256, address) {
+    return (a * b, address(this));
+}
+}
