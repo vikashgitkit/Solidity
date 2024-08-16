67b63911b3f6810466f6cb625595ee8c128f6dfc
Webskitters <vikashofficial9@gmail.com>
2024-08-16 16:56:01 +0530

    Declares constructor

--- Solidity_Examples/35_ballot/Ballot.sol
+++ Solidity_Examples/35_ballot/Ballot.sol
@@ -19,4 +19,29 @@ contract Ballot {
     bytes32 name;  // short name (up to 32 bytes)
     uint256 voteCount; // number of accumulated votes
   }
+  address public chairperson;
+
+     // This declares a state variable that stores a `Voter` struct for each possible address.
+     mapping(address => Voter) public voters;
+
+     // A dynamically-sized array of `Proposal` structs.
+     Proposal[] public proposals;
+
+    // Create a new ballot to choose one of `proposalNames`
+    constructor(bytes32[] memory proposalNames) {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 1;
+       
+         // For each of the provided proposal names, create a new proposal object and add it to the end of the array.
+         for(uint256 i = 0; i < proposalNames.length; i++) {
+            // `Proposal({...})` creates a temporary
+            // Proposal object and `proposals.push(...)`
+            // appends it to the end of `proposals`.
+            proposals.push(Proposal({name: proposalNames[i], 
+            voteCount: 0}));
+         }
+
+    }
+
+    
 }
