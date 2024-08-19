9e2db7497dd422479e55c0f22a55d754b0bc96d2
Webskitters <vikashofficial9@gmail.com>
2024-08-19 16:58:38 +0530

    Swapping

--- Solidity_Examples/37_uniswap_simple_swap/Uniswap_simple_swap.sol
+++ Solidity_Examples/37_uniswap_simple_swap/Uniswap_simple_swap.sol
@@ -8,5 +8,14 @@ import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 
 contract SimpleSwap{
-    
+
+    ISwapRouter public immutable swapRouter;
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    uint24 public constant feeTier = 3000;
+
+    constructor(ISwapRouter _swapRouter) {
+        swapRouter = _swapRouter;
+    }
+
 }
