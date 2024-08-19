88403cebd4858ac3eb07fd7aa24103990ba24ea1
Webskitters <vikashofficial9@gmail.com>
2024-08-19 16:58:13 +0530

    Swapping

---
+++ Solidity_Examples/37_uniswap_simple_swap/Uniswap_simple_swap.sol
@@ -0,0 +1,12 @@
+// https://uniswap.org/blog/your-first-uniswap-integration
+
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity =0.8.9;
+pragma abicoder v2;
+
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+
+contract SimpleSwap{
+    
+}
