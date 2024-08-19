edffd374658d720daf8a6e0f12b8fc36b53ed3e6
Webskitters <vikashofficial9@gmail.com>
2024-08-19 16:54:34 +0530

    Complete vault code

--- Solidity_Examples/36_vault/Vault.sol
+++ Solidity_Examples/36_vault/Vault.sol
@@ -8,34 +8,32 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Vault is ERC20{
-
+contract Vault is ERC20 {
     IERC20 public immutable token;
 
-    constructor(IERC20 token_) ERC20("Liquidity Provider Token", "LP-TKN"){
+    constructor(IERC20 token_) ERC20("Liquidity Provider Token", "LP-TKN") {
         token = token_;
     }
 
-
-     function deposit(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
+    function deposit(uint256 amount) external {
+        require(amount > 0);
 
         uint256 amountToken = amount;
-        uint256 supplyToken = this.totalSupply();
+        uint256 supplyLPToken = this.totalSupply();
         uint256 balanceToken = token.balanceOf(address(this));
 
         uint256 amountLPToken;
-        if(supplyToken == 0) {
+        if (supplyLPToken == 0) {
             amountLPToken = amountToken;
-        } else  {
-            amountLPToken = (amountToken * supplyToken) / balanceToken;
+        } else {
+            amountLPToken = (amountToken * supplyLPToken) / balanceToken;
         }
 
         _mint(msg.sender, amountLPToken);
         token.transferFrom(msg.sender, address(this), amountToken);
-     }
+    }
 
-      function withdraw(uint256 amount) external {
+    function withdraw(uint256 amount) external {
         require(amount > 0);
 
         uint256 amountLPToken = amount;
@@ -46,6 +44,13 @@ contract Vault is ERC20{
 
         _burn(msg.sender, amountLPToken);
         token.transfer(msg.sender, amountToken);
-     }
+    }
+}
 
+contract AssetToken is ERC20, Ownable {
+    constructor() ERC20("Asset Token", "AST") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
