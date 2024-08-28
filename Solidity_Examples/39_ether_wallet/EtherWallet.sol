661dbfac61550d974c10e54c65a9d2ecdca83d59
Webskitters <vikashofficial9@gmail.com>
2024-08-28 15:42:13 +0530

    Ether wallet

---
+++ Solidity_Examples/39_ether_wallet/EtherWallet.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+/*
+https://solidity-by-example.org/app/ether-wallet
+
+An example of a basic wallet.
+
+  Anyone can send ETH.
+  Only the owner can withdraw.
+*/
+
+pragma solidity ^0.8.9;
