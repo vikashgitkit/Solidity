13702d0c378f0277b1bc47dca0aa5d9033d8f4e1
Webskitters <vikashofficial9@gmail.com>
2024-08-28 15:51:11 +0530

    Multi sig wallet events

---
+++ Solidity_Examples/40_multi_sig_wallet/MultiSigWallet.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+/*
+https://solidity-by-example.org/app/multi-sig-wallet
+
+Let's create an multi-sig wallet. Here are the specifications.
+
+The wallet owners can:
+  submit a transaction
+  approve and revoke approval of pending transcations
+  anyone can execute a transcation after enough owners has approved it.
+*/
+
+pragma solidity ^0.8.20;
+
+contract MultiSigWallet{
+
+     event Deposit(address indexed sender, uint256 amount, uint256 balance);
+    event SubmitTransaction(
+        address indexed owner,
+        uint256 indexed txIndex,
+        address indexed to,
+        uint256 value,
+        bytes data
+    );
+    event ConfirmTransaction(address indexed owner, uint256 indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint256 indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint256 indexed txIndex);
+
+
+}
