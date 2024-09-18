c6891b6f1d9914a403a22c94718664177aa4e192
Webskitters <vikashofficial9@gmail.com>
2024-09-18 19:08:23 +0530

    Setting up the pair abi

--- Solidity_Examples/42_pancackSwap/src/AbiInfo.js
+++ Solidity_Examples/42_pancackSwap/src/AbiInfo.js
@@ -1,1 +1,10 @@
 const erc20ABI=["function decimals() public view returns(uint8)"]
+
+//it will return the address of liquidity provider contract
+//0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16
+const factoryABI=["function getPair(address tokenA, address tokenB) external view returns (address pair)"]
+
+const paiABI=[
+    "function token0() external view returns (address)",
+    "function token1() external view returns (address)"
+]
