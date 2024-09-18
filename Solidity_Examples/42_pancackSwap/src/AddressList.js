61e39f7075b5573f74a2fe3b49a2158a684390fc
Webskitters <vikashofficial9@gmail.com>
2024-09-18 17:09:06 +0530

    Setting up the contract addresses

---
+++ Solidity_Examples/42_pancackSwap/src/AddressList.js
@@ -0,0 +1,11 @@
+const factoryAddress="";
+const routerAddress="";
+const fromAddress=""; //BUSD
+const toAddress=""; //WBNB
+
+module.exports={
+    factoryAddress,
+    routerAddress,
+    fromAddress,
+    toAddress
+}
