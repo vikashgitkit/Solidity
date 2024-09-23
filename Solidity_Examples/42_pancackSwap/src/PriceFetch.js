bec42e4dee89f555d0a3edb7388d80bb4f3a8976
Webskitters <vikashofficial9@gmail.com>
2024-09-23 12:40:27 +0530

    Fetching price

--- Solidity_Examples/42_pancackSwap/src/PriceFetch.js
+++ Solidity_Examples/42_pancackSwap/src/PriceFetch.js
@@ -1,55 +1,48 @@
 const ethers = require('ethers');
-const{factoryAddress,
-    routerAddress,
-    fromAddress,
-    toAddress} = require("./AddressList");
+const { factoryAddress, routerAddress, fromAddress, toAddress } = require("./AddressList");
 
-    const {
-        erc20ABI,
+const {
+    erc20ABI,
     factoryABI,
     paiABI,
     routerABI
-    } = require("./AbiInfo")
+} = require("./AbiInfo")
 
-    const provider = new ethers.providers.JsonRpcProvider(
-        "https://bsc-dataseed1.binance.org/"
+const provider = new ethers.providers.JsonRpcProvider(
+    "https://bsc-dataseed1.binance.org/"
+)
+
+const factoryInstance = new ethers.Contract(
+    factoryAddress, factoryABI, provider
+)
+
+const routerInstance = new ethers.Contract(
+    routerAddress, routerABI, provider
+)
+
+const priceFetch = async (humanFormat) => {
+    const token1 = new ethers.Contract(
+        fromAddress, erc20ABI, provider
     )
 
-    const factoryInstance = new ethers.Contract(
-        factoryAddress, factoryABI, provider
-    )
-    //console.log("factory instance is:", factoryInstance);
-
-    const routerInstance = new ethers.Contract(
-        routerAddress, routerABI, provider
+    const token2 = new ethers.Contract(
+        toAddress, erc20ABI, provider
     )
 
-    const priceFetch=async(humanFormat)=>{
-        const token1 = new ethers.Contract(
-            fromAddress, erc20ABI, provider
-        )
+    const decimal1 = await token1.decimals()
+    const decimal2 = await token2.decimals()
 
-        const token2 = new ethers.Contract(
-            toAddress, erc20ABI, provider
-        )
+    const amountIn = ethers.utils.parseUnits(humanFormat, decimal1).toString();
 
-        const decimal1 = await token1.decimals()
-        const decimal2 = await token2.decimals()
-        //console.log("Decimal is:", decimal1)
-        const amountIn = ethers.utils.parseUnits(humanFormat, decimal1).toString();
+    // Pass the amountIn and the token addresses as an array
+    const amountsOut = await routerInstance.getAmountsOut(amountIn, [fromAddress, toAddress]);
 
-        const amountsOut = await routerInstance.getAmountsOut(amountIn,{
-            fromAddress,
-            toAddress
-        })
+    const humanOutput = ethers.utils.formatUnits(
+        amountsOut[1].toString(),
+        decimal2
+    )
+    console.log("HumanOutPut:", humanOutput)
+}
 
-        const humanOutput = ethers.utils.formatUnits(
-            amountsOut[1].toString(),
-            decimal2
-        )
-        console.log("HumanOutPut:", humanOutput)
-    }
-
-    humanFormat="100"
-    priceFetch(humanFormat)
-
+humanFormat = "100"
+priceFetch(humanFormat)
