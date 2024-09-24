8f4600347b5c10989e2fdb3ace6a475b0cdd2c5f
Webskitters <vikashofficial9@gmail.com>
2024-09-24 12:33:05 +0530

    Imported interfaces and libraries

--- Solidity_Examples/43_flashLoan_smartContract/contracts/FlashLoan.sol
+++ Solidity_Examples/43_flashLoan_smartContract/contracts/FlashLoan.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
-// import "hardhat/console.sol";
+// Uniswap interface and library imports
+import "./interfaces/IUniswapV2Factory.sol";
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IUniswapV2Router01.sol";
+import "./interfaces/IUniswapV2Router02.sol";
+import "./interfaces/IERC20.sol";
+import "./libraries/UniswapV2Library.sol";
+import "./libraries/SafeERC20.sol";
+import "hardhat/console.sol";
 
 contract FlashLoan {
 
