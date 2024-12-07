aabeab32ddd80efdf2689eaca8c9a34a6a22765e
Vikash <vikashofficial9@gmail.com>
2024-12-07 12:30:56 +0530

    DelegateCall

---
+++ Solidity_Examples/45_smart_contract_programmer/21_DelegateCall.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract TestDelegateCall {
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(uint _num) external payable {
+        num = 2 * _num;
+        sender = msg.sender;
+        value = msg.value;
+    }
+}
+
+//So using delegateCall as we are calling the setVars fn of
+//testDelegateCall contract but the value will be change in the DelegateCall contract not in TestDelegateCall contract
+contract DelegateCall {
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(address _test, uint _num) external payable {
+        // _test.delegatecall(
+        //     abi.encodeWithSignature("setVars(uint256)", _num)
+        // );
+
+        //Below line also do the same thing
+        (bool success, bytes memory data) = _test.delegatecall(
+            abi.encodeWithSelector(TestDelegateCall.setVars.selector, _num)
+        );
+        require(success, "DelegateCall failed");
+    }
+}
