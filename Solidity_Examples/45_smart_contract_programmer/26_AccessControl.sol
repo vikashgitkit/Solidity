11099ae05cd962adf666533bfc8e83544edb4bd2
Vikash <vikashofficial9@gmail.com>
2024-12-08 00:20:29 +0530

    Access controll

---
+++ Solidity_Examples/45_smart_contract_programmer/26_AccessControl.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract AccessControl {
+    event GrantRole(bytes32 indexed role, address indexed account);
+    event RevokeRole(bytes32 indexed role, address indexed account);
+
+    //role => account => bool
+    mapping(bytes32 => mapping(address => bool)) public roles;
+
+    bytes32 private constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
+    bytes32 private constant USER = keccak256(abi.encodePacked("USER"));
+
+    modifier onlyRole(bytes32 _role) {
+        require(roles[_role][msg.sender], "Not authorized");
+        _;
+    }
+
+    function _grantRole(bytes32 _role, address _account) internal {
+        roles[_role][_account] = true;
+        emit GrantRole(_role, _account);
+    }
+}
