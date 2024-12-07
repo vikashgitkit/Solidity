89326b634ca02d02a129556040044ec7ab479436
Vikash <vikashofficial9@gmail.com>
2024-12-07 23:53:32 +0530

    verify signature

--- Solidity_Examples/45_smart_contract_programmer/25_VerifySig.sol
+++ Solidity_Examples/45_smart_contract_programmer/25_VerifySig.sol
@@ -21,4 +21,42 @@ contract VerifySig {
 
         return recover(ethSignedMessageHash, _sig) == _signer;
     }
+
+    function getMessageHash(
+        string memory _message
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_message));
+    }
+
+    function getEthSignedMessageHash(
+        bytes32 _messageHash
+    ) public pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked(
+                    "\x19Ethereum Signed Message:\n32",
+                    _messageHash
+                )
+            );
+    }
+
+    function recover(
+        bytes32 _ethSignedMessageHash,
+        bytes memory _sig
+    ) public pure returns (address) {
+        (bytes32 r, bytes32 s, uint8 v) = _split(_sig);
+        return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
+
+    function _split(
+        bytes memory _sig
+    ) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(_sig.length == 65, "Invalid signature length");
+
+        assembly {
+            r := mload(add(_sig, 32))
+            s := mload(add(_sig, 64))
+            v := byte(0, mload(add(_sig, 96)))
+        }
+    }
 }
