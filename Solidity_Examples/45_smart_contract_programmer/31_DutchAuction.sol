b1359d963be7539b20bd1453e7c2df3c20623c0e
Vikash <vikashofficial9@gmail.com>
2024-12-14 21:03:03 +0530

    Dutch Auction

--- Solidity_Examples/45_smart_contract_programmer/31_DutchAuction.sol
+++ Solidity_Examples/45_smart_contract_programmer/31_DutchAuction.sol
@@ -11,9 +11,30 @@ contract DutchAuction {
     IERC721 public immutable nft;
     uint public immutable nftId;
 
-    address public immutable seller;
+    address payable public immutable seller;
     uint public immutable startingPrice;
     uint public immutable startAt;
     uint public immutable expiresAt;
     uint public immutable discountRate;
+
+    constructor(
+        uint _startingPrice,
+        uint _discountRate,
+        address _nft,
+        uint _nftId
+    ) {
+        seller = payable(msg.sender);
+        startingPrice = _startingPrice;
+        discountRate = _discountRate;
+        startAt = block.timestamp;
+        expiresAt = block.timestamp + DURATION;
+
+        require(
+            _startingPrice >= _discountRate * DURATION,
+            "Starting price < discount"
+        );
+
+        nft = IERC721(_nft);
+        nftId = _nftId;
+    }
 }
