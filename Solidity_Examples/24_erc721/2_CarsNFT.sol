c97f0af4cae421d669b738d920581133b9b95979
vikash sharma <vikashofficial9@gmail.com>
2025-03-08 20:02:42 +0530

    Creating latest version of erc721 contract

---
+++ Solidity_Examples/24_erc721/2_CarsNFT.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.24;
+
+//https://medium.com/coinmonks/how-to-deploy-and-interact-with-an-erc-721-smart-contract-on-abstract-using-hardhat-and-0e3d14c7cd6a
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract CarsNft is ERC721, ERC721Enumerable, ERC721URIStorage {
+    uint256 private _nextTokenId;
+
+    constructor()
+        ERC721("CarsNft", "CARS")
+    {}
+
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+  
+}
