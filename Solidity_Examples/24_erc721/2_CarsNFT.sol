c17656e69ef8d5f377d219b2ef81d1c65be70f83
vikash sharma <vikashofficial9@gmail.com>
2025-03-08 20:04:16 +0530

    Creating latest version of erc721 contract

--- Solidity_Examples/24_erc721/2_CarsNFT.sol
+++ Solidity_Examples/24_erc721/2_CarsNFT.sol
@@ -46,5 +46,12 @@ contract CarsNft is ERC721, ERC721Enumerable, ERC721URIStorage {
         return super.tokenURI(tokenId);
     }
 
-    
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        
+    }
 }
