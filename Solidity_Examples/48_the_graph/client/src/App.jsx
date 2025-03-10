1ef779c55c34ba53b2549721dee1e5ab7a0c4f59
vikash sharma <vikashofficial9@gmail.com>
2025-03-10 19:15:08 +0530

    Fetching data using subgraph

---
+++ Solidity_Examples/48_the_graph/client/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import {createClient} from 'urql';
+
+function App() {
+  const [tokens, setTokens] = useState([]);
+
+  const queryUrl = "https://gateway.thegraph.com/api/94d2b9f620df1630edfa50b53f323b07/subgraphs/id/HUZDsRpEVP2AvzDCyzDHtdc64dyDxx8FQjzsmqSg4H3B";
+  const query = `{
+  factories(first: 5) {
+    id
+    poolCount
+    txCount
+    totalVolumeUSD
+  }
+}`
+
+ 
+  return (
+    <>
+      <div>
+      </div>
+    </>
+  )
+}
+
+export default App
